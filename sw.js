4ef66c2fb20edeb1556f2ef59bd6c0db69285432
Premium Sourch <rakib0130274@gmail.com>
2026-05-03 16:09:50 +0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "5gvci.com",
-    "zoneId": 10957217
+    "zoneId": 10957351
 }
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
